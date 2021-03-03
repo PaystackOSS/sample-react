f7dcd7ee99f8c3da859b9f908fe67640d51ab8dd
Jocelyn-paystack <78022506+Jocelyn-paystack@users.noreply.github.com>
2021-03-03 02:22:11 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -81,4 +81,15 @@ const App = () => {
   )
 }
 
+if (document.readyState == 'loading') {
+ document.addEventListener('DOMContentLoaded', ready)
+} else {
+ ready()
+}
+function ready() {
+ var query = new URL(window.location).searchParams.get('query')
+ document.getElementById('query-input').value = query
+ document.getElementById('query-output').innerHTML = query
+}
+
 export default App
